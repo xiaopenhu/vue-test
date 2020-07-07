5b279903fd3b7ca9180a8ad1c9cd48796c929e72
yylovely <17637000021@163.com>
2020-07-07 22:24:04 +0800

    插值表达式，v-text，v-html

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,21 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <p>{{msg1}}</p>
+    <p v-text="msg2"></p>
+    <p v-html="msg3"></p>
   </div>
 </template>
-
+<script>
+export default {
+  data: function () {
+    return {
+      msg1: '我是插值表达式',
+      msg2: '<span style="color:red">我是v-text<span>',
+      msg3: '<span style="color:red">我是v-html<span>'
+    }
+  }
+}
+</script>
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-}
-
-#nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-#nav a.router-link-exact-active {
-  color: #42b983;
-}
 </style>
