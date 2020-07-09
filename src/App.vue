3ccc1aa1de1b26ae108e2caaa728dae51b193680
yylovely <17637000021@163.com>
2020-07-09 22:25:14 +0800

    v-bind/v-if/v-else-if/v-else

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,16 @@
 <template>
   <div id="app">
-    <div class="guding">
-      <div style="width: 34%"></div>
-      <div style="width: 11%" @click="change('/image/index.jpg')"></div>
-      <div style="width: 18%" @click="change('/image/sxy.jpg')"></div>
-      <div style="width: 15%"  @click="change('/image/hezuo.jpg')"></div>
-      <div style="width: 21%" @click="change('/image/lianxi.jpg')"></div>
-    </div>
-    <div class="header">
-      <img src="/image/header.jpg" alt="" width="100%">
-    </div>
-    <div class="index">
-      <img :src="imgName" alt="" width="100%">
-    </div>
-    <div class="footer">
-      <img src="/image/footer.jpg" alt="" width="100%">
+    <div>
+    <p :class="{ a: isA,b: isB}">使用 class</p>
+    <p :class="[black,yellow]">使用 class (数组)</p>
+    <p :style="styleDate"> 使用 style</p>
+    <p v-bind:style="styleDate">使用</p>
+  </div>
+    <div>
+      <p v-if="type === 'a'">A</p>
+      <p v-else-if="type === 'b'">B</p>
+      <p v-else-if="type === 'c'">C</p>
+      <p v-else>other</p>
     </div>
   </div>
 </template>
@@ -22,41 +18,19 @@
 export default {
   data: function () {
     return {
-      imgName: '/image/index.jpg'
-    }
-  },
-  methods: {
-    change: function (name) {
-      this.imgName = name
+      isA: true,
+      isB: false,
+      black: 'cblack',
+      yellow: 'cyellow',
+      styleDate: {
+        fontSize: '40px',
+        color: 'red',
+        backgroundColor: '#ccc',
+        type: 'a'
+      }
     }
   }
 }
-document.addEventListener('DOMContentLoaded', () => {
-  const html = document.querySelector('html')
-  const fontSize = window.innerWidth / 10
-  html.style.fontSize = fontSize + 'px'
-})
 </script>
 <style>
-  .guding{
-    height: 1rem;
-    width: 100%;
-    position: absolute;
-    display: flex;
-  }
-  .guding div{
-    height: 100%;
-  }
-  .header{
-    height: auto;
-    wight:100%;
-  }
-  .footer{
-    height: auto;
-    width: 100%;
-  }
-  .index{
-    height: auto;
-    width: 100%;
-  }
 </style>
