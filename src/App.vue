074d7d25cb69ada255b7d65a423fa2c93b551531
yylovely <17637000021@163.com>
2020-07-10 19:20:29 +0800

    pors父组件向子组件传值

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,18 @@
 <template>
   <div id="app">
-    <div>
-    <p :class="{ a: isA,b: isB}">使用 class</p>
-    <p :class="[black,yellow]">使用 class (数组)</p>
-    <p :style="styleDate"> 使用 style</p>
-    <p v-bind:style="styleDate">使用</p>
-  </div>
-    <div>
-      <p v-if="type === 'a'">A</p>
-      <p v-else-if="type === 'b'">B</p>
-      <p v-else-if="type === 'c'">C</p>
-      <p v-else>other</p>
-    </div>
+    <HelloWorld :aa="msg"></HelloWorld>
   </div>
 </template>
 <script>
+import HelloWorld from './components/HelloWorld'
 export default {
   data: function () {
     return {
-      isA: true,
-      isB: false,
-      black: 'cblack',
-      yellow: 'cyellow',
-      styleDate: {
-        fontSize: '40px',
-        color: 'red',
-        backgroundColor: '#ccc',
-        type: 'a'
-      }
+      msg: '父组件传递给了我，所以我是子组件'
     }
+  },
+  components: {
+    HelloWorld
   }
 }
 </script>
-<style>
-</style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,17 +1,22 @@
 <template>
-  <div class="hello"></div>
+  <div class="hello">
+    <p>我是子组件hello world</p>
+    <p>{{aa}}</p>
+  </div>
 </template>
 
 <script>
 export default {
   name: 'HelloWorld',
-  props: {
-    msg: String
-  }
+  props: ['aa']
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-
+.hello{
+  width: 200px;
+  height: 200px;
+  background: red;
+}
 </style>
